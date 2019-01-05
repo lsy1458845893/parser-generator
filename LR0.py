9097ac43656809567e0b7294951e9fefe79e81c2
刘世宇 <1458845893@qq.com>
2019-01-06 00:42:46 +0800

    add LR(0) example

--- LR0.py
+++ LR0.py
@@ -79,7 +79,7 @@ class Table:
     def filter(self, target):
         items_list = [itemset.items for itemset in self.itemsets]
         if target.items in items_list:
-            return items_list.index(target.items)
+            return self.itemsets[items_list.index(target.items)]
         else:
             self.itemsets.append(target)
             return target
@@ -90,3 +90,50 @@ class Table:
 
 table = Table([0])
 print(table)
+
+stream = ['1', '*', '0', '+', '1']
+cursor = 0
+
+def next():
+    global cursor, stream
+    if cursor < len(stream):
+        tok = stream[cursor]
+        cursor += 1
+        return tok
+    else:
+        return 'EOF'
+
+
+def apply(target, itemset):
+    global cursor, stream
+    stack = [next()]
+    state = [itemset]
+    while True:
+        stack_top = stack[len(stack) - 1]
+        state_top = state[len(state) - 1]
+        if stack_top == target:
+            break
+        if stack_top not in state_top.transition:
+            raise Exception(" %s not in %s" %
+                            (stack_top, str(list(state_top.transition))))
+
+        itemset = state_top.transition[stack_top]
+        if not itemset.transition:  # reduce only
+            print('reduce', [s.index for s in state], stack, stream[cursor:])
+            items = itemset.items
+            item = items[0]
+            assert(len(items) == 1)
+            assert(len(rules[item[0]]) == item[1] + 1)
+            size = len(rules[item[0]]) - 2
+            for i in range(size):
+                stack.pop()
+                state.pop()
+            stack.pop()
+            stack.append(rules[item[0]][0])
+        else:  # shift , may have reduce but ignore it
+            print('shift', [s.index for s in state], stack, stream[cursor:])
+            stack.append(next())
+            state.append(state_top.transition[stack_top])
+    print(stack)
+
+apply('S', table.itemsets[0])
